94717bdcea4ccd783b716634fea359792f4ab4e6
dinesh-404 <khuntidnsh@gmail.com>
2023-04-07 21:58:10 +0530

    final push

--- calculate.js
+++ calculate.js
@@ -5,18 +5,27 @@ let clrbtn = document.getElementById("clr");
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener("click", () => {
     if (ans.value == 0) {
-      ans.value = btn[i].value;
-    } else if (btn[i].value == "+") {
+      if(btn[i].value=="DEL"){
+        ans.value=0;
+      }
+      else{
+        ans.value=btn[i].value;
+      }
+    } 
+    else if (btn[i].value == "+") {
       ans.value += btn[i].value;
-    } else if (btn[i].value == "=") {
+    } 
+    else if (btn[i].value == "=") {
       ans.value = eval(ans.value);
-    } else if (btn[i].value == "DEL") {
+    } 
+    else if (btn[i].value == "DEL") {
       if (ans.value.length > 1) {
         ans.value = ans.value.slice(0, -1);
       } else {
         ans.value = 0;
       }
-    } else {
+    } 
+    else {
       ans.value += btn[i].value;
     }
   });
